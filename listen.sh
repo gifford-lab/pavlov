7e9a1bc0455bb130ab844f092e7dcf75860e917d
Matt Edwards <mttdwrds@gmail.com>
2014-09-02 19:39:06 -0400

    Changed mp3 to wav.

--- listen.sh
+++ listen.sh
@@ -6,7 +6,7 @@ while true; do
     count=`wget -qq -O - "http://lists.csail.mit.edu/pipermail/vultures/2014/date.html" | grep -oP "<b>Messages:</b> \d+<p>" | cut -f 2 -d " " | cut -f 1 -d "<"`
     if [ $count -ne $lastcount ]; then
 	echo "new! now at "$count
-	aplay bell-ringing-04.mp3
+	aplay bell-ringing-04.wav
     fi
     lastcount=$count
     sleep 10s

d7e8515ad8484b8024b76de611a914c8771fa4ac
Matt Edwards <mttdwrds@gmail.com>
2014-09-02 19:37:10 -0400

    Initial commit.

---
+++ listen.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+count=-1
+lastcount=-1
+
+while true; do
+    count=`wget -qq -O - "http://lists.csail.mit.edu/pipermail/vultures/2014/date.html" | grep -oP "<b>Messages:</b> \d+<p>" | cut -f 2 -d " " | cut -f 1 -d "<"`
+    if [ $count -ne $lastcount ]; then
+	echo "new! now at "$count
+	aplay bell-ringing-04.mp3
+    fi
+    lastcount=$count
+    sleep 10s
+done
+

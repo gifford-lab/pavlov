7cd9caed89dc30a20f0c8ddad12adb1ba334ab7f
Matt Edwards <mttdwrds@gmail.com>
2014-09-02 19:46:55 -0400

    Added dynamic year support (zomg).

--- listen.sh
+++ listen.sh
@@ -3,7 +3,8 @@ count=-1
 lastcount=-1
 
 while true; do
-    count=`wget -qq -O - "http://lists.csail.mit.edu/pipermail/vultures/2014/date.html" | grep -oP "<b>Messages:</b> \d+<p>" | cut -f 2 -d " " | cut -f 1 -d "<"`
+    year=`date | cut -f 7 -d " "`
+    count=`wget -qq -O - "http://lists.csail.mit.edu/pipermail/vultures/${year}/date.html" | grep -oP "<b>Messages:</b> \d+<p>" | cut -f 2 -d " " | cut -f 1 -d "<"`
     if [ $count -ne $lastcount ]; then
 	echo "new! now at "$count
 	mplayer bell-ringing-04.mp3
